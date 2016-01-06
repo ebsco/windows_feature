48792b591b839c55df063f4e18b16ae43d9d4ca2
mcallb <mcallb@gmail.com>
2016-01-06 09:19:57 -0500

    Update metadata.rb
    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          'All rights reserved'
 description      'Provides an LWRP that facilitates access to the mixlibrary-core gem windows feature support.'
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 
-version          '1.0.1' #Automatically versioned by CI system.  This version will always be out of date.
+version          '1.0.23' #Automatically versioned by CI system.  This version will always be out of date.
 
